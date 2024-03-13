81a2a7b8f208f15e51e9e6eceb607ca5bd790b92
Harry Rhodes <hjtrhodes@gmail.com>
2024-03-13 13:46:11 +0000

    setting up routing

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to Worldwise</h1>
+      <p>This is the homepage of our website.</p>
+    </div>
+  );
+}
+
+export default Homepage;
