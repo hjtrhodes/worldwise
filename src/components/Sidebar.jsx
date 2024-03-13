c5577bf8141730ad621e2e0f8338b0429610919e
Harry Rhodes <hjtrhodes@gmail.com>
2024-03-13 16:47:38 +0000

    Main app page navigation working with nested routes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,14 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
+      <Outlet />
 
       <p>List of Cities</p>
       <Footer />
