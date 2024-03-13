3fd23a3aa4555d9741826a9bdae419cc0b689544
Harry Rhodes <hjtrhodes@gmail.com>
2024-03-13 16:26:32 +0000

    routing in place and app being built

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
-import PageNav from "./components/PageNav";
+import Login from "./pages/Login";
+import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <PageNav />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,26 @@
-function Homepage() {
+import { Link } from "react-router-dom";
+import styles from "./Homepage.module.css";
+import PageNav from "../components/PageNav";
+
+export default function Homepage() {
   return (
-    <div>
-      <h1>Welcome to Worldwise</h1>
-    </div>
+    <main className={styles.homepage}>
+      <PageNav />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/app" className="cta">
+          Start Tracking Now
+        </Link>
+      </section>
+    </main>
   );
 }
-
-export default Homepage;
