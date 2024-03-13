9856c1718318bf444780808a175c30fbca778c00
Harry Rhodes <hjtrhodes@gmail.com>
2024-03-13 15:35:00 +0000

    made navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import PageNav from "./components/PageNav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <PageNav />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,6 @@ function Homepage() {
   return (
     <div>
       <h1>Welcome to Worldwise</h1>
-      <p>This is the homepage of our website.</p>
     </div>
   );
 }
